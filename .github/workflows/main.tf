ca38c673f2aa0e6b868cf57147e2eaab0881c53a
Sarah Oguche <153545236+SarahUO@users.noreply.github.com>
2024-06-30 23:50:13 -0400

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 # Create public subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = "vpc-05b0ca6c03e94aa26"
-  cidr_block        = "10.0.6.0/24"  # Updated CIDR block
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = var.aws_region
   tags = {
     Name = "Public Subnet"
   }
@@ -15,9 +15,9 @@ resource "aws_subnet" "public_subnet" {
 
 # Create private subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id            = "vpc-05b0ca6c03e94aa26"
-  cidr_block        = "10.0.5.0/24"  # Updated CIDR block
-  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.aws_region
   tags = {
     Name = "Private Subnet"
   }
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_security_group" "public_sg" {
   name        = "public_sg"
   description = "Allow SSH and HTTP"
-  vpc_id      = "vpc-05b0ca6c03e94aa26"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -55,13 +55,13 @@ resource "aws_security_group" "public_sg" {
 resource "aws_security_group" "private_sg" {
   name        = "private_sg"
   description = "Allow internal traffic"
-  vpc_id      = "vpc-05b0ca6c03e94aa26"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = [aws_subnet.private_subnet.cidr_block]
   }
 
   egress {
@@ -74,7 +74,7 @@ resource "aws_security_group" "private_sg" {
 
 # NAT Gateway and EIP
 resource "aws_eip" "nat_eip" {
-  domain = "vpc"
+  vpc      = true
 }
 
 resource "aws_nat_gateway" "nat" {
@@ -84,11 +84,11 @@ resource "aws_nat_gateway" "nat" {
 
 # Public route table
 resource "aws_route_table" "public_rt" {
-  vpc_id = "vpc-05b0ca6c03e94aa26"
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "igw-0256891a23a381290"
+    gateway_id = aws_internet_gateway.main.id
   }
 
   tags = {
@@ -103,7 +103,7 @@ resource "aws_route_table_association" "public_rt_assoc" {
 
 # Private route table
 resource "aws_route_table" "private_rt" {
-  vpc_id = "vpc-05b0ca6c03e94aa26"
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -172,91 +172,31 @@ resource "aws_secretsmanager_secret" "db_credentials" {
 resource "aws_secretsmanager_secret_version" "db_credentials_version" {
   secret_id     = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode({
-    username = "admin"
+    username = "admin",
     password = "your_password"
   })
 }
 
+# Bastion host instance
 resource "aws_instance" "bastion" {
-  ami           = "ami-01e076d5c9e040974 # Example Amazon Linux 2 AMI
+  ami           = "ami-01e076d5c9e040974" # Example Amazon Linux 2 AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.public_sg.id]
-
-  key_name = "sarahkeypair2"
 
   tags = {
     Name = "Bastion Host"
   }
 }
 
+# Application server instance
 resource "aws_instance" "app_server" {
   ami           = "ami-01e076d5c9e040974" # Example Amazon Linux 2 AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private_subnet.id
   security_groups = [aws_security_group.private_sg.id]
 
-  key_name = "sarahkeypair2"
-
   tags = {
     Name = "App Server"
   }
-}
-
-# IAM Role for developers
-resource "aws_iam_role" "developer_role" {
-  name = "developer_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-# IAM Policy for developers
-resource "aws_iam_policy" "developer_policy" {
-  name = "developer_policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-          "s3:ListBucket",
-          "s3:GetObject"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-# Attach policy to role
-resource "aws_iam_role_policy_attachment" "developer_attach" {
-  role       = aws_iam_role.developer_role.name
-  policy_arn = aws_iam_policy.developer_policy.arn
-}
-
-
-# Secrets Manager for storing credentials
-resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "db_credentials"
-}
-
-resource "aws_secretsmanager_secret_version" "db_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.db_credentials.id
-  secret_string = jsonencode({
-    username = "admin"
-    password = "your_password"
-  })
 }
