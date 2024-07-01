6d878b02db987fd9aa00868e2321071d2eb00396
Sarah Oguche <153545236+SarahUO@users.noreply.github.com>
2024-06-30 23:50:47 -0400

    Update variables.tf

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "The AWS region to create resources in."
   default     = "us-east-1"
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet."
+  default     = "10.0.6.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet."
+  default     = "10.0.5.0/24"
+}
